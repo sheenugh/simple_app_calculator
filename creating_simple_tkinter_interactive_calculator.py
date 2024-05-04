da427b3ed58feb927621c0e41bf460689adbccc1
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-05-04 18:51:33 +0800

    Done pputting the instruction in txt and pseudo code.

---
+++ creating_simple_tkinter_interactive_calculator.py
@@ -0,0 +1,20 @@
+# Delima, Sheena Mae D.
+# BSCPE 1-2
+# May 04, 2024
+
+
+# ------------------------------------------------------
+
+
+# ========== PSEUDO CODE ==========
+# || LIBRARIES \ PACKAGERS ||
+
+# || IMPORTS || 
+
+# || ACTUAL CODES ||
+# - Root tkinter and window's size
+# - Ask user about the operation.
+# - Ask the the 66user of two numbers.
+# - Try and except category.
+# - If,elif,else code.
+# - Tkinter mainloop
