e02282de87d08c41e192dc3fc250c3db87af9f8a
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-05-04 19:30:08 +0800

    Revising the comments, font size and style, and additing code for sub, multip,div operation.

--- creating_simple_tkinter_interactive_calculator.py
+++ creating_simple_tkinter_interactive_calculator.py
@@ -20,23 +20,36 @@ window.geometry("500x300")
 
 # - ASK THE USER FOR THE TWO NUMBERS.
 # - Creating the label 1 and entry 1 for user to input his/her desired number.
-num1 = Label(window, text = "Enter your first number:", font=("Arial", 15))
+num1 = Label(window, text = "Enter your first number:", font=("times", 18))
 num1.pack()
 num1_entry_widget = Entry(window, font=("Helvetica", 12))
 num1_entry_widget.pack()
 
 # - Creating the label 2 and entry 2 for user to input his/her desired number.
-num2 = Label(window, text = "Enter your first number:", font=("Arial", 15))
+num2 = Label(window, text = "Enter your first number:", font=("times", 18))
 num2.pack()
 num2_entry_widget = Entry(window, font=("Helvetica", 12))
 num2_entry_widget.pack()
 
-
 # - ASK USER FOR THE OPERATION
-# - # - Creating the label 1 and entry 1 for user to input his/her desired number.
-button_label = Label(window, text = "What operation do you want to use?", font=("Arial", 15))
+# - Creating a label for asking the user of his/her desired operation.
+button_label = Label(window, text = "What operation do you want to use?", font=("times", 17))
 button_label.pack()
-
+# - Button for operations.
+frame = Frame(window) 
+frame.pack(pady=10)
+# - Addition button.
+addition_button =Button(frame, text="Addition", font=("serif", 11))
+addition_button.pack()
+# - Subtraction button.
+subtraction_button =Button(frame, text="Subtraction", font=("serif", 11))
+subtraction_button.pack()
+# - Multiplication button.
+multiplication_button =Button(frame, text="Multiplication", font=("serif", 11))
+multiplication_button.pack()
+# - Division button.
+division_button =Button(frame, text="Division", font=("serif", 11))
+division_button.pack()
 
 # - Try and except category.
 # - If,elif,else code.
