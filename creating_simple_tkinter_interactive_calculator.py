162d3c3fe7e87bdc06e9ea91e1d33b75604dcd9e
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-05-04 19:07:53 +0800

    Adding code for label 1.

--- creating_simple_tkinter_interactive_calculator.py
+++ creating_simple_tkinter_interactive_calculator.py
@@ -18,8 +18,14 @@ window = Tk()
 window.title("Calculator")
 window.geometry("500x300")
 
+# - Creating a label to input the user's desired number.
+num1 = Label(window, text = "Enter your first number:", font=("Arial", 15))
+num1.pack()
+
+# - Ask the the user of two numbers 
+
 # - Ask user about the operation.
-# - Ask the the 66user of two numbers.
+
 # - Try and except category.
 # - If,elif,else code.
 # - Tkinter mainloop
